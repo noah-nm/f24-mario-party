1d3a323433efe030c28e7914ca2ce0142fdfc5fd
Cobblestone <aidenhperry@gmail.com>
2024-12-16 10:44:39 -0500

    Add Controller Support (#9)
    * add inital controller support
    
    * abstract controller init to method
    
    * fix library issues and add pov
    
    * triggers are actually the same axis
    
    ---------
    
    Co-authored-by: Perry <PERRYA@students.dsbn.org>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,56 @@
+import java.util.ArrayList;
+
+import DLibX.DConsole;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+import utils.Gamepad;
+
 public class App {
+    ArrayList<Gamepad> gamepads = new ArrayList<>();
+    Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App app = new App();
+        app.run();
+    }
+
+    public void run() {
+        // init controllers
+        initGameControllers();
+
+        while (true) {
+            // ask controller for new data
+            for (Controller controller : controllers) {
+                controller.poll();
+            }
+
+            // handle input below
+
+            System.out.println("Triggers" + gamepads.get(0).getTriggers());
+
+            // if (gamepads.get(0).getAButton()) {
+            //     System.out.println("A is being pressed");
+            // }
+
+            DConsole.pause(10);
+        }
+    }
+
+    /**
+     * Initializes game controllers for use
+     * 
+     */
+    public void initGameControllers() {
+        // get all controllers
+
+        // find gamepads
+        for (Controller controller : controllers) {
+
+            if (controller.getType() == Controller.Type.GAMEPAD) {
+                System.out.println(controller.getName() + " is a gamepad");
+                // add gamepad to list
+                gamepads.add(new Gamepad(controller));
+            }
+        }
     }
 }
---
+++ src/utils/Gamepad.java
@@ -0,0 +1,141 @@
+package utils;
+
+import net.java.games.input.Component;
+import net.java.games.input.Component.Identifier;
+import net.java.games.input.Controller;
+
+public class Gamepad {
+    private Controller controller;
+
+    public Gamepad(Controller c) {
+        this.controller = c;
+    }
+
+    /**
+     * Gets if a button is pressed
+     * 
+     * @param button the button identifier
+     * @return button is down
+     */
+    public boolean getButtonPressed(Identifier button) {
+
+        Component[] components = controller.getComponents();
+
+        for (Component component : components) {
+
+            if (component.getIdentifier().toString().equals(button.toString())) {
+                return component.getPollData() > 0.5f; // Adjust threshold as needed
+            }
+        }
+        return false;
+    }
+
+    public boolean getAButton() {
+        return getButtonPressed(Identifier.Button._0);
+    }
+
+    public boolean getBButton() {
+        return getButtonPressed(Identifier.Button._1);
+    }
+
+    public boolean getXButton() {
+        return getButtonPressed(Identifier.Button._2);
+    }
+
+    public boolean getYButton() {
+        return getButtonPressed(Identifier.Button._3);
+    }
+
+    /**
+     * Returns if the top left back paddle is pressed
+     * 
+     * @return button is down
+     */
+    public boolean getLeftPaddleButton() {
+        return getButtonPressed(Identifier.Button._4);
+    }
+
+    /**
+     * Returns if the top right back paddle is pressed
+     * 
+     * @return button is down
+     */
+    public boolean getRightPaddleButton() {
+        return getButtonPressed(Identifier.Button._5);
+    }
+
+    /**
+     * Returns if the back/select button is pressed
+     * 
+     * @return button is down
+     */
+    public boolean getBackButton() {
+        return getButtonPressed(Identifier.Button._6);
+    }
+
+    /**
+     * Returns if the start button is pressed
+     * 
+     * @return button is down
+     */
+    public boolean getStartButton() {
+        return getButtonPressed(Identifier.Button._7);
+    }
+
+    /**
+     * Returns if L3/left stick is pressed
+     * 
+     * @return button is down
+     */
+    public boolean getLeftPushStickButton() {
+        return getButtonPressed(Identifier.Button._8);
+    }
+
+    /**
+     * Returns if R3/right stick is pressed
+     * 
+     * @return button is down
+     */
+    public boolean getRightPushStickButton() {
+        return getButtonPressed(Identifier.Button._9);
+    }
+
+    public double getLeftStickX() {
+        return controller.getComponent(Identifier.Axis.X).getPollData();
+    }
+
+    public double getLeftStickY() {
+        return controller.getComponent(Identifier.Axis.Y).getPollData();
+    }
+
+    public double getRightStickX() {
+        return controller.getComponent(Identifier.Axis.RX).getPollData();
+    }
+
+    public double getRightStickY() {
+        return controller.getComponent(Identifier.Axis.RY).getPollData();
+    }
+
+    /**
+     * Returns how far the triggers are pressed down
+     * 
+     * It doesn't fully reach 1, so use something smaller for a threshold
+     
+     * If its positive, right is pressed down, 
+     * If its negative, left is down
+     * 
+     * @return ~ -1.0 to ~1.0
+     */
+    public double getTriggers() {
+        return controller.getComponent(Identifier.Axis.Z).getPollData();
+    }
+
+    /**
+     * Returns the DPad/POV
+     * 
+     * @return 0 - 1
+     */
+    public double getPOV() {
+        return controller.getComponent(Identifier.Axis.POV).getPollData();
+    }
+}
