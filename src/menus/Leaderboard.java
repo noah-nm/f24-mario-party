2d7d3b734ba381ef54827208af96e81467341b72
noah-nm <noah.jr.mills@gmail.com>
2025-01-13 09:51:48 -0500

    Leaderboard rework

--- src/menus/Leaderboard.java
+++ src/menus/Leaderboard.java
@@ -1,103 +1,60 @@
 package menus;
+
 import utils.Gamepad;
 import DLibX.*;
 import games.Game;
-import java.awt.*;  
-import net.java.games.input.Controller; 
- 
+import java.awt.*;
+import net.java.games.input.Controller;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Leaderboard extends Game {
-    
+
     public Leaderboard(DConsole dc, Gamepad[] gamepads, Controller[] controllers, int[] scores) {
         super(dc, gamepads, controllers, scores);
     }
 
-    public void play(){
+    public void play() {
+        int[] workingScores = Arrays.copyOf(scores, 4);
+        Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
+        Font arialMedium = new Font("arial", Font.BOLD, 40);
 
-        int time = 250;
-        int index1 = 0;
-        int index2 = 0;
-        int index3 = 0;
-        int index4 = 0;
-        int s1 = 0;
-        int s2 = 0;
-        int s3 = 0;
-        int s4 = 0;
-        int[] workingScores = this.scores; 
-        Color[] co = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
-        Font arialMedium = new Font("arial", 1, 40);  
+        HashMap<Integer, Integer> indexToSortedValue = new HashMap<>();
 
-        //find winner
-        for(int i = 0; i<4; i++){
-            if(workingScores[i]>index1){
-                index1 = workingScores[i];
-                s1 = i;
-            }
+        // Store original indexes and values in a temporary array
+        Integer[] originalIndexes = new Integer[workingScores.length];
+        for (int i = 0; i < workingScores.length; i++) {
+            originalIndexes[i] = i;
         }
-        index1 = s1;
-        workingScores[index1] = 0;
 
-        for(int i = 0; i<4; i++){
-            if(workingScores[i]>index2){
-                index2 = workingScores[i];
-                s2 = i;
-            }
+        // Sort the indexes array based on the values in the scores array
+        Arrays.sort(originalIndexes, (a, b) -> Integer.compare(workingScores[b], workingScores[a]));
+
+        // Create the mapping of sorted indexes to values
+        for (int i = 0; i < originalIndexes.length; i++) {
+            indexToSortedValue.put(i, workingScores[originalIndexes[i]]);
         }
-        index2 = s2;
-        workingScores[index2] = 0;
 
-        for(int i = 0; i<4; i++){
-            if(workingScores[i]>index3){
-                index3 = workingScores[i];
-                s3 = i;
-            }
-        }
-        index3 = s3;
-        workingScores[index3] = 0;
+        dc.clear();
 
-        for(int i = 0; i<4; i++){
-            if(workingScores[i]>index4){
-                index4 = workingScores[i];
-                s4 = i;
-            }
-        }
-        index4 = s4;
-        workingScores[index4] = 0;
+        // Draw the leaderboard GUI
+        dc.setFont(arialMedium);
 
-        //for 5 sec
-        while (time > 0) {
-        
-            //Draw GUI
-            dc.setFont(arialMedium);
-            dc.setPaint(co[index1]);
-            dc.fillRect(100, 100, 1000, 80);
+        for (int rank = 0; rank < originalIndexes.length; rank++) {
+            int playerIndex = originalIndexes[rank];
+            int yPosition = 100 + rank * 120;
+
+            // Set the background color for each rank
+            dc.setPaint(colors[playerIndex]);
+            dc.fillRect(100, yPosition, 1000, 80);
+
+            // Draw the player name and score
             dc.setPaint(Color.BLACK);
-            dc.drawString("Player" + (index1+1), 150, 110);
-            dc.drawString(scores[index1], 950, 110);
-
-            dc.setPaint(co[index2]);
-            dc.fillRect(100, 220, 1000, 80);
-            dc.setPaint(Color.BLACK);
-            dc.drawString("Player" + (index2+1), 150, 230);
-            dc.drawString(scores[index2], 950, 230);
-
-            dc.setPaint(co[index3]);
-            dc.fillRect(100, 340, 1000, 80);
-            dc.setPaint(Color.BLACK);
-            dc.drawString("Player" + (index3+1), 150, 350);
-            dc.drawString(scores[index3], 950, 350);
-
-            dc.setPaint(co[index4]);
-            dc.fillRect(100, 460, 1000, 80);
-            dc.setPaint(Color.BLACK);
-            dc.drawString("Player" + (index4+1), 150, 470);
-            dc.drawString(scores[index4], 950, 470);
-
-            dc.pause(20);
-            dc.redraw();
-            time--;
+            dc.drawString("Player " + (playerIndex + 1), 150, yPosition + 10);
+            dc.drawString(String.valueOf(workingScores[playerIndex]), 950, yPosition + 10);
         }
-        
 
+        dc.redraw();
+        dc.pause(5000);
     }
-
 }
